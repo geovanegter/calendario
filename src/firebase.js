0fd1e7410f741b25b5ef623f5c52e1791372cb7b
geovanegter <48461670+geovanegter@users.noreply.github.com>
2025-07-08 16:14:55 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyC6_BRRbrR7NDb2a9QPkTR5oz0o9FjcNa4",
+  authDomain: "agenda-ggmw.firebaseapp.com",
+  databaseURL: "https://agenda-ggmw-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "agenda-ggmw",
+  storageBucket: "agenda-ggmw.firebasestorage.app",
+  messagingSenderId: "411608997404",
+  appId: "1:411608997404:web:94bfcfea00cdd1d1e909d9",
+  measurementId: "G-NTZNKKWQYJ"
 };
 
 const app = initializeApp(firebaseConfig);
