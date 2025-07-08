0a3144febdfbd319424b15cd3139421a740dee4a
geovanegter <48461670+geovanegter@users.noreply.github.com>
2025-07-08 16:46:02 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -13,4 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export default db;
